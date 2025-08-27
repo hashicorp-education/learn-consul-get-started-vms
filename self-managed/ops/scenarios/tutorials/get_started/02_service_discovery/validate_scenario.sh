6d779ca246e4d5b5007c7af8de8f5f36c41776d9
danielehc <daniele@hashicorp.com>
2025-08-27 12:49:21 +0200

    Investigate Service discovery

--- self-managed/ops/scenarios/tutorials/get_started/02_service_discovery/validate_scenario.sh
+++ self-managed/ops/scenarios/tutorials/get_started/02_service_discovery/validate_scenario.sh
@@ -142,8 +142,6 @@ log "All Consul servers correctly working."
 header2 "Check Consul client configuration."
 # ==============================================================================
 
-set -x
-
 NODES_ARRAY=( "hashicups-db" "hashicups-api" "hashicups-frontend" "hashicups-nginx" )
 
 for node in "${NODES_ARRAY[@]}"; do
@@ -171,11 +169,6 @@ for node in "${NODES_ARRAY[@]}"; do
 		-p ${CONSUL_DNS_PORT} \
 		${node}.service.${CONSUL_DATACENTER}.${CONSUL_DOMAIN} | \
 		wc -l`
-
-	dig +short \
-		@consul-server-0 \
-		-p ${CONSUL_DNS_PORT} \
-		${node}.service.${CONSUL_DATACENTER}.${CONSUL_DOMAIN}
 
 	OUTP=$?
 
@@ -206,8 +199,6 @@ for node in "${NODES_ARRAY[@]}"; do
 	fi
 
 done
-
-set +x 
 
 log_info "Test for scenario passed. Great job."
 exit 0
